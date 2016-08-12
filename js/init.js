52007b061283e3833b4bad0c29c60f27893b8e40
jinmylopez <jinmy.osiris@gmail.com>
2016-08-12 02:21:55 -0500

    primer commit

--- js/init.js
+++ js/init.js
@@ -3,6 +3,9 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('.carousel').carousel();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
